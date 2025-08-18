6dcdef41a0e8b59d6d1834bb50f798d2e536a8c0
Kevin Jou <kevjou97@gmail.com>
2025-08-17 21:11:25 -0700

    Update views

--- clinical_scheduler/config/settings/development.py
+++ clinical_scheduler/config/settings/development.py
@@ -161,3 +161,5 @@ EMAIL_USE_TLS = env('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='noreply@andiehsialmft.com')
+
+CONTACT_EMAIL = 'Andie.hsia.lfmt@gmail.com'
