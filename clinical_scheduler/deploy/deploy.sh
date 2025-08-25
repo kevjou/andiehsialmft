11fd317a456e101a189e680afc006d41d3cdea5c
Kevin Jou <kevjou97@gmail.com>
2025-08-25 00:01:01 -0700

    Update deploy.sh

--- clinical_scheduler/deploy/deploy.sh
+++ clinical_scheduler/deploy/deploy.sh
@@ -3,7 +3,7 @@
 # Deployment script
 set -e
 
-APP_DIR="/var/www/clinical_scheduler/andiehsialmft"
+APP_DIR="/var/www/clinical_scheduler/andiehsialmft/clinical_scheduler"
 USER="clinical_scheduler"
 
 echo "Starting deployment..."
